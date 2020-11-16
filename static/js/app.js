15ad240199d4a85319cf3ded86cdd7276b068262
Alex Riner <rineralexander@gmail.com>
2020-11-16 15:04:31 -0600

    dropdown working now

--- static/js/app.js
+++ static/js/app.js
@@ -4,8 +4,12 @@ d3.json("samples.json").then(function(data) {
     // dropdownMenu.append("option").text("1");
     // dropdownMenu.append("option").text("2");
     // dropdownMenu.append("option").text("3");
-    data.names.forEach(name=>dropdownMenu.append("option").text(`${name}`));
+    data.names.forEach(name=>dropdownMenu.append("option")
+            .text(`${name}`)
+            .attr("value",name));
 
-    
+    dropdownMenu.on("change",function(){
+        console.log(this.value);
+    });
 });
 

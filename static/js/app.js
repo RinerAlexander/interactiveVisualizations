734155438ce347c6b0925ae19562a67ef0df248a
Alex Riner <rineralexander@gmail.com>
2020-11-16 12:00:06 -0600

    still trying witht the dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,11 @@
 d3.json("samples.json").then(function(data) {
-    console.log(data.names);
+    // console.log(data.names);
     var dropdownMenu = d3.select("#selDataset");
-    dropdownMenu.append("option").text("1");
-    dropdownMenu.append("option").text("2");
-    dropdownMenu.append("option").text("3");
+    // dropdownMenu.append("option").text("1");
+    // dropdownMenu.append("option").text("2");
+    // dropdownMenu.append("option").text("3");
+    data.names.forEach(name=>dropdownMenu.append("option").text(`${name}`));
+
+    
 });
+

274b490cbb9ab4b095aa98f330b86b7f11b6c048
Alex Riner <rineralexander@gmail.com>
2020-11-16 08:50:43 -0600

    getting drop down menu to work

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+d3.json("samples.json").then(function(data) {
+    console.log(data.names);
+    var dropdownMenu = d3.select("#selDataset");
+    dropdownMenu.append("option").text("1");
+    dropdownMenu.append("option").text("2");
+    dropdownMenu.append("option").text("3");
+});

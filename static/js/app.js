4ca3e27c325d140653d1e812836b26a704c4624b
Alex Riner <rineralexander@gmail.com>
2020-11-17 18:06:57 -0600

    getting graph to work

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,45 @@
 d3.json("samples.json").then(function(data) {
-    // console.log(data.names);
-    var dropdownMenu = d3.select("#selDataset");
-    // dropdownMenu.append("option").text("1");
-    // dropdownMenu.append("option").text("2");
-    // dropdownMenu.append("option").text("3");
+
+    //Add id's to dropdown menu
+    var dropdownMenu = d3.select("#selDataset");   
     data.names.forEach(name=>dropdownMenu.append("option")
             .text(`${name}`)
             .attr("value",name));
 
+    //Change info/ graphs when a selection is made
     dropdownMenu.on("change",function(){
+
+        //change demographic info
         mData=data.metadata.filter(entry => entry.id==this.value);
         mData=mData[0];
         console.log(mData);
         var demaInfo = d3.select("#sample-metadata");
+        demaInfo.text("");
         Object.entries(mData).forEach(([i,d])=>demaInfo.append("p")
             .text(`${i}: ${d}`));
+
+        //bar graph
+        sData=data.samples.filter(entry => entry.id==this.value);
+        sData=sData[0];
+        values=sData.sample_values;
+        ids=sData.otu_ids.map(entry => String(entry));
+        labels=sData.otu_labels;
+
+        console.log(values,ids);
+
+        trace1 = {
+            x:labels,
+            y:values,
+            type:"bar"
+        };
+
+        input=[trace1]
+
+        var layout = {
+            title: "'Bar' Chart"
+          };
+          
+        Plotly.newPlot("bar", input, layout);
     });
 
 });

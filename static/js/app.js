c372bdcce4ee58ab1943a093291ff7b34539f4f8
Alex Riner <rineralexander@gmail.com>
2020-11-18 10:54:58 -0600

    page initializes with first patient

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,18 @@
+var dropdownMenu = d3.select("#selDataset");
+
+//Add id's to dropdown menu
 d3.json("samples.json").then(function(data) {
-
-    //Add id's to dropdown menu
-    var dropdownMenu = d3.select("#selDataset");   
     data.names.forEach(name=>dropdownMenu.append("option")
-            .text(`${name}`)
-            .attr("value",name));
+        .text(`${name}`)
+        .attr("value",name));
+});
 
+function updatePage(patient){
+    d3.json("samples.json").then(function(data) {
     //Change info/ graphs when a selection is made
-    dropdownMenu.on("change",function(){
 
         //change demographic info
-        mData=data.metadata.filter(entry => entry.id==this.value);
+        mData=data.metadata.filter(entry => entry.id==patient);
         mData=mData[0];
         console.log(mData);
         var demaInfo = d3.select("#sample-metadata");
@@ -19,29 +21,40 @@ d3.json("samples.json").then(function(data) {
             .text(`${i}: ${d}`));
 
         //bar graph
-        sData=data.samples.filter(entry => entry.id==this.value);
+        sData=data.samples.filter(entry => entry.id==patient);
         sData=sData[0];
-        values=sData.sample_values;
-        ids=sData.otu_ids.map(entry => String(entry));
-        labels=sData.otu_labels;
+        values=sData.sample_values.slice(0,10);
+        ids=sData.otu_ids.map(entry => String(entry)).slice(0,10);
+        labels=sData.otu_labels.slice(0,10);
 
         console.log(values,ids);
 
         trace1 = {
             x:ids,
             y:values,
-            type:"bar"
+            type:"bar",
+            text:labels
         };
-
         input=[trace1]
 
         var layout = {
             title: "'Bar' Chart",
             xaxis:{type:"category"}
-          };
+        };
           
         Plotly.newPlot("bar", input, layout);
-    });
 
+    });
+}
+
+dropdownMenu.on("change",function(){
+    updatePage(this.value);
 });
 
+//Initilize page
+d3.json("samples.json").then(function(data) {
+    data.names.forEach(name=>dropdownMenu.append("option")
+        .text(`${name}`)
+        .attr("value",name));
+});
+updatePage(940);

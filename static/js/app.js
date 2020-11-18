5bf5a3e56edfbd957d62a3aed1f9dcba70c7d3b3
Alex Riner <rineralexander@gmail.com>
2020-11-18 11:43:19 -0600

    making sure everything is saved

--- static/js/app.js
+++ static/js/app.js
@@ -12,7 +12,7 @@ function updatePage(patient){
     //Change info/ graphs when a selection is made
 
         //change demographic info
-        mData=data.metadata.filter(entry => entry.id==patient);
+        var mData=data.metadata.filter(entry => entry.id==patient);
         mData=mData[0];
         console.log(mData);
         var demaInfo = d3.select("#sample-metadata");
@@ -20,29 +20,48 @@ function updatePage(patient){
         Object.entries(mData).forEach(([i,d])=>demaInfo.append("p")
             .text(`${i}: ${d}`));
 
-        //bar graph
-        sData=data.samples.filter(entry => entry.id==patient);
+        //Collect variables for graphs
+        var sData=data.samples.filter(entry => entry.id==patient);
         sData=sData[0];
-        values=sData.sample_values.slice(0,10);
-        ids=sData.otu_ids.map(entry => String(entry)).slice(0,10);
-        labels=sData.otu_labels.slice(0,10);
+        var values=sData.sample_values.slice(0,10);
+        var ids=sData.otu_ids.map(entry => String(entry)).slice(0,10);
+        var labels=sData.otu_labels.slice(0,10);
 
-        console.log(values,ids);
-
-        trace1 = {
+        //bar graph
+        var trace1 = {
             x:ids,
             y:values,
             type:"bar",
             text:labels
         };
-        input=[trace1]
+        var input=[trace1];
 
         var layout = {
-            title: "'Bar' Chart",
-            xaxis:{type:"category"}
+            xaxis:{type:"category",
+                    title:"OTU ID"},
+            yaxis:{title:"Sample Value"}
         };
           
         Plotly.newPlot("bar", input, layout);
+
+        //Bubble Chart
+        trace1 = {
+            x: ids,
+            y: values,
+            mode: 'markers',
+            marker: {
+              size: values
+            },
+            text:labels
+          };
+        input=[trace1];
+        
+        var layout = {
+            xaxis:{title:"OTU ID's"},
+            yaxis:{title:"Sample Value"}
+          };
+        
+        Plotly.newPlot("bubble",input,layout);
 
     });
 }
